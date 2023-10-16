cd1dc3089f1526b2dbd24604817b622750921e87
Matthew Hasenjager <hasenjag27@gmail.com>
2023-10-16 17:22:48 -0400

    Modified for comparing seed metrics

--- DiffusionCurve_DataProcessing_Parallelized.R
+++ DiffusionCurve_DataProcessing_Parallelized.R
@@ -9,8 +9,8 @@ diffDataList <- importCSVs(path = "/home/mhasenja/scratch/SA_HyperNets/Run7/Sim-
 
 #Create folder in which to store simulation results
 run_ID=strftime(Sys.time(), format="d3%Y%m%d%H%M%S")
-sim_diffCurveData="Sim-diffCurveData_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats"
-sim_T50="Sim-T50_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats"
+sim_diffCurveData="Sim-diffCurveData_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats_NetMets"
+sim_T50="Sim-T50_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats_NetMets"
 if(!file.exists(sim_diffCurveData)) dir.create(sim_diffCurveData)
 if(!file.exists(sim_T50)) dir.create(sim_T50)
 
@@ -29,7 +29,13 @@ diffusionCurveData <- data.frame("uniqueID" = rep(s, maxTime),
                                  "groupEffect" = 0,
                                  "timeStep" = 1:maxTime,
                                  "propInformed" = 0,
-                                 "meanSeedAge" = 0)
+                                 "meanSeedAge" = 0, 
+                                 "meanSeedDegree" = 0,
+                                 "meanSeedBetween" = 0,
+                                 "meanSeedStrength" = 0,
+                                 "meanSeedsiD" = 0,
+                                 "meanSeedsiBC" = 0,
+                                 "meanSeedSED" = 0)
 
   diffusionCurveData$topologyID <- diffDataTemp$simID[1]
   diffusionCurveData$ageBias <- diffDataTemp$ageBias[1]
@@ -41,6 +47,14 @@ diffusionCurveData <- data.frame("uniqueID" = rep(s, maxTime),
   diffusionCurveData$propInformed <- sapply(seq(from = 1, to = maxTime), function(x) nrow(diffDataTemp[which(diffDataTemp$acquisitionTime <= x & diffDataTemp$acquisitionTime > 0),])/initialNaive)
   initDemons <- diffDataTemp[which(diffDataTemp$initDemons == 1),]$ID
   diffusionCurveData$meanSeedAge <- mean(sapply(initDemons, function(x) sum(x < diffDataTemp$ID)/99))
+  diffusionCurveData$meanSeedDegree <- mean(sapply(initDemons, function(x) sum(diffDataTemp[which(diffDataTemp$ID == x),]$degree > diffDataTemp$degree)/99))
+  diffusionCurveData$meanSeedBetween <- mean(sapply(initDemons, function(x) sum(diffDataTemp[which(diffDataTemp$ID == x),]$betweenness > diffDataTemp$betweenness)/99))
+  diffusionCurveData$meanSeedStrength <- mean(sapply(initDemons, function(x) sum(diffDataTemp[which(diffDataTemp$ID == x),]$strength > diffDataTemp$strength)/99))
+  diffusionCurveData$meanSeedsiD <- mean(sapply(initDemons, function(x) sum(diffDataTemp[which(diffDataTemp$ID == x),]$siD < diffDataTemp$siD)/99))
+  diffusionCurveData$meanSeedsiBC <- mean(sapply(initDemons, function(x) sum(diffDataTemp[which(diffDataTemp$ID == x),]$siD < diffDataTemp$siBC)/99))
+  diffusionCurveData$meanSeedSED <- mean(sapply(initDemons, function(x) sum(diffDataTemp[which(diffDataTemp$ID == x),]$subEdgeDens > diffDataTemp$subEdgeDens)/99))
+  
+  
 
   diffusionCurveData_T50 <- data.frame(
     "uniqueID" = diffusionCurveData$uniqueID[1],
@@ -51,7 +65,13 @@ diffusionCurveData <- data.frame("uniqueID" = rep(s, maxTime),
     "seedStrategy" = diffusionCurveData$seedStrategy[1],
     "groupEffect" = diffusionCurveData$groupEffect[1],
     "T50" = min(diffusionCurveData[which(diffusionCurveData$propInformed >= 0.5),]$timeStep), 
-    "meanSeedAge" = diffusionCurveData$meanSeedAge[1]
+    "meanSeedAge" = diffusionCurveData$meanSeedAge[1],
+    "meanSeedDegree" = diffusionCurveData$meanSeedDegree[1],
+    "meanSeedBetween" = diffusionCurveData$meanSeedBetween[1],
+    "meanSeedStrength" = diffusionCurveData$meanSeedStrength[1],
+    "meanSeedsiD" = diffusionCurveData$meanSeedsiD[1],
+    "meanSeedsiBC" = diffusionCurveData$meanSeedsiBC[1],
+    "meanSeedSED" = diffusionCurveData$meanSeedSED[1]
   )
   
   write.csv(diffusionCurveData, file = file.path(sim_diffCurveData, sprintf("simData_%s_%.01i.csv", run_ID, s)))
