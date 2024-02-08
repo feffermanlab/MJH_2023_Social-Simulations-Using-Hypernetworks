76641c5f51519ea51f438e2ceca81f797377489e
Matthew Hasenjager <hasenjag27@gmail.com>
2024-02-08 13:04:47 -0500

    Update path

--- DiffusionCurve_DataProcessing_Parallelized.R
+++ DiffusionCurve_DataProcessing_Parallelized.R
@@ -4,7 +4,7 @@ library(doParallel)
 
 registerDoParallel(cores = 20)
 
-diffDataList <- importCSVs(path = "~/scratch/SA_HyperNets/Run7/Sim-details_edgeListContagion_Weights_AgeStrats/")
+diffDataList <- importCSVs(path = "~/scratch/SA_HyperNets/Run7/Sim-details_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats_withGSPref/")
 
 #Create folder in which to store simulation results
 run_ID=strftime(Sys.time(), format="d3%Y%m%d%H%M%S")
