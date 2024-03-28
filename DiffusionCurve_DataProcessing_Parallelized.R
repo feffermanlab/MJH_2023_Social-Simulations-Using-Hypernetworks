e078b784541690a839ce07d5931cb4c67d5de008
Matthew Hasenjager <hasenjag27@gmail.com>
2024-03-28 09:18:36 -0400

    Path updates

--- DiffusionCurve_DataProcessing_Parallelized.R
+++ DiffusionCurve_DataProcessing_Parallelized.R
@@ -4,12 +4,12 @@ library(doParallel)
 
 registerDoParallel(cores = 20)
 
-diffDataList <- importCSVs(path = "~/scratch/SA_HyperNets/Run7/Sim-details_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats_Mar24/")
+diffDataList <- importCSVs(path = "~/scratch/SA_HyperNets/Run7/Sim-details_higherOrderContagion_Weights_GoGvsHyp_NetMets_Mar24/")
 
 #Create folder in which to store simulation results
 run_ID=strftime(Sys.time(), format="d3%Y%m%d%H%M%S")
-sim_diffCurveData="Sim-diffCurveData_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats_Mar24"
-sim_T50="Sim-T50_higherOrderContagion_Weights_GoGvsHyp_paramSweep_AgeStrats_Mar24"
+sim_diffCurveData="Sim-diffCurveData_higherOrderContagion_Weights_GoGvsHyp_paramSweep_NetMets_Mar24"
+sim_T50="Sim-T50_higherOrderContagion_Weights_GoGvsHyp_paramSweep_NetMets_Mar24"
 if(!file.exists(sim_diffCurveData)) dir.create(sim_diffCurveData)
 if(!file.exists(sim_T50)) dir.create(sim_T50)
 
