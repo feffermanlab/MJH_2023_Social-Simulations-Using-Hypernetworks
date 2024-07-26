4e410c923360211be80d017b3d0ead6db9af5b41
Matthew Hasenjager <hasenjag27@gmail.com>
2024-07-26 16:14:10 -0400

    Modified to run for indirect centrality metrics

--- IC_Monograph_DiffusionModel.R
+++ IC_Monograph_DiffusionModel.R
@@ -25,18 +25,20 @@ colnames(incidMat_C) <- 1:ncol(incidMat_C)
 
 #Create folder in which to store simulation results
 run_ID=strftime(Sys.time(), format="d3%Y%m%d%H%M%S")
-sim_details="Sim-details_contagionResults_DirectMetrics_July11"
+sim_details="Sim-details_contagionResults_IndirectMetrics_July26"
 if(!file.exists(sim_details)) dir.create(sim_details)
 
 #Set random seed to ensure repeatability
-set.seed(07112024)
+#set.seed(07112024)
+set.seed(07262024)
 
 #Set simulation-wide parameters
 initialInformed = c(1,3,6,10,20)
 lambda = 0.025
 
 #Set target seed strategy set and names
-seedStrategySet <- c("Degree", "Strength", "sDeg_Avg", "sDeg_Avg_G", "random")
+seedStrategySet <- c("Betweenness","Betweenness_binary","sBC_Avg","sBC_Avg_G","random")
+  #"Degree", "Strength", "sDeg_Avg", "sDeg_Avg_G", "random")
 
 #Determines whether probability of learning decreases within increasing group/hyperedge size
 groupInterferenceEffect <- c("groupSizeIndependent")
@@ -45,7 +47,7 @@ groupInterferenceEffect <- c("groupSizeIndependent")
 socialReinforcementValues <- c(1.1, 3)
 
 #Set number of replicates per condition
-r = 500
+r = 50
 
 currentIDs <- rownames(incidMat_C)
 
