c74f6a6a06f3ac0f4e6b59253441bfdfb7dd1811
Matthew Hasenjager <hasenjag27@gmail.com>
2024-07-31 10:23:27 -0400

    Updated for InVS15 Direct Metrics

--- IC_Monograph_DiffusionModel.R
+++ IC_Monograph_DiffusionModel.R
@@ -25,8 +25,8 @@ colnames(incidMat_C) <- 1:ncol(incidMat_C)
 
 #Create folder in which to store simulation results
 run_ID=strftime(Sys.time(), format="d3%Y%m%d%H%M%S")
-sim_details="Sim-details_contagionResults_IndirectMetrics_InVS15"
-#sim_details="Sim-details_contagionResults_DirectMetrics_InVS15"
+#sim_details="Sim-details_contagionResults_IndirectMetrics_InVS15"
+sim_details="Sim-details_contagionResults_DirectMetrics_InVS15"
 if(!file.exists(sim_details)) dir.create(sim_details)
 
 #Set random seed to ensure repeatability
@@ -40,8 +40,8 @@ lambda = 0.025
 
 #Set target seed strategy set and names
 seedStrategySet <- c(
-  "Betweenness","Betweenness_binary","sBC_Avg","sBC_Avg_G","random")
-  #"Degree", "Strength", "sDeg_Avg", "sDeg_Avg_G", "random")
+  #"Betweenness","Betweenness_binary","sBC_Avg","sBC_Avg_G","random")
+  "Degree", "Strength", "sDeg_Avg", "sDeg_Avg_G", "random")
 
 #Determines whether probability of learning decreases within increasing group/hyperedge size
 groupInterferenceEffect <- c("groupSizeIndependent")
