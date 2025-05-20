c8157d1533fcfd2ee5fcc9f8506527f61e24d83b
Matthew Hasenjager <hasenjag27@gmail.com>
2025-05-20 10:53:13 -0400

    Initial commit

---
+++ hypernetwork_group_size_vs_composition.R
@@ -0,0 +1,305 @@
+
+#Load packages
+source("~/git/MJH_2023_Social-Simulations-Using-Hypernetworks/hypernetwork_group_size_vs_composition.R", chdir = TRUE)
+#source("hypernetwork_group_size_vs_composition_simFunctions.R")
+library(igraph)
+library(asnipe)
+library(data.table)
+library(foreach)
+library(doParallel)
+library(stringr)
+
+registerDoParallel(cores = 20)
+
+#setwd("C://Users/matth/Desktop/Multilayer Hypernetworks/")
+
+#Create folder in which to store simulation results
+run_ID=strftime(Sys.time(), format="d3%Y%m%d%H%M%S")
+sim_indData="Sim_individual_level_data"
+sim_summaryData="Sim_summary_data"
+sim_diffSummaryData = "Sim_diffusion_summary_data"
+sim_netData_dyNet1 = "Sim_dyadic_network_1"
+sim_netData_hypNet1 = "Sim_higherOrder_network_1"
+sim_netData_dyNet2 = "Sim_dyadic_network_2"
+sim_netData_hypNet2 = "Sim_higherOrder_network_2"
+sim_netData_dyNet3 = "Sim_dyadic_network_3"
+sim_netData_hypNet3 = "Sim_higherOrder_network_3"
+
+if(!file.exists(sim_indData)) dir.create(sim_indData)
+if(!file.exists(sim_summaryData)) dir.create(sim_summaryData)
+if(!file.exists(sim_diffSummaryData)) dir.create(sim_diffSummaryData)
+if(!file.exists(sim_netData_dyNet1)) dir.create(sim_netData_dyNet1)
+if(!file.exists(sim_netData_hypNet1)) dir.create(sim_netData_hypNet1)
+if(!file.exists(sim_netData_dyNet2)) dir.create(sim_netData_dyNet2)
+if(!file.exists(sim_netData_hypNet2)) dir.create(sim_netData_hypNet2)
+if(!file.exists(sim_netData_dyNet3)) dir.create(sim_netData_dyNet3)
+if(!file.exists(sim_netData_hypNet3)) dir.create(sim_netData_hypNet3)
+
+##Set seed for reproducibility
+set.seed(5162025)
+
+#Set social transmission rate; determines baseline probability of learning from an active neighbor in the dyadic layer
+social_trans <- 0.1
+
+#Possible radii for determinig subgroups
+rad <- c(0.15, 0.25, 0.35)
+
+#Distributions for dominance ranks
+domDist <- c("domUni", "domExp")
+
+resourceAvailability <- c(0.25, 0.5, 0.75, 1)
+
+groupAdjust <- c("FALSE", "TRUE")
+
+###################################################
+
+#Set number of sims per condition
+nSims = 250
+
+foreach(s = 1:nSims) %dopar% {
+  #for(s in 1:nSims) {
+  
+  ##Generate individuals in family/kinship groups
+  #For the moment, not using families in model, but keeping in case of expanding
+  ind_data <- generate_population(popSize = 100, nInitInformed = 1)
+  n_indivs <- nrow(ind_data)
+  ind_data$simID <- s
+  
+  ind_data$domUni <- runif(nrow(ind_data), min = 0, max = 1)
+  domExp <- sort(rexp(nrow(ind_data), rate = 2))
+  domExp <- sapply(rank(ind_data$domUni), function(x) domExp[x])
+  ind_data$domExp <- (domExp - min(domExp))/(max(domExp) - min(domExp))
+  ind_data$domExp <- ifelse(ind_data$domExp == 0, min(ind_data[which(ind_data$domExp > 0),]$domExp)/2, ind_data$domExp)
+  
+  ind_dataOrig <- ind_data
+  
+  ##Create spatial proximity network and hypergraphs
+  netList <- generate_latent_space_multilayer_hypergraph(ind_data = ind_data, r = rad)
+  
+  write.csv(netList[[1]], file.path(sim_netData_dyNet1, sprintf("simData_%s_%.01i.csv", run_ID, s)))
+  write.csv(netList[[2]], file.path(sim_netData_hypNet1, sprintf("simData_%s_%.01i.csv", run_ID, s)))
+  write.csv(netList[[3]], file.path(sim_netData_dyNet2, sprintf("simData_%s_%.01i.csv", run_ID, s)))
+  write.csv(netList[[4]], file.path(sim_netData_hypNet2, sprintf("simData_%s_%.01i.csv", run_ID, s)))
+  write.csv(netList[[5]], file.path(sim_netData_dyNet3, sprintf("simData_%s_%.01i.csv", run_ID, s)))
+  write.csv(netList[[6]], file.path(sim_netData_hypNet3, sprintf("simData_%s_%.01i.csv", run_ID, s)))
+  
+  for(d in 1:length(domDist)) {
+    for(R in resourceAvailability) {
+      for(r in c(1,3,5)) {
+        for(g in groupAdjust) {
+        
+        #Reset individual-level data to initial values
+        ind_data <- ind_dataOrig
+        
+        domDistribution <- domDist[d]
+        ind_data$distUsed <- domDistribution
+        ind_data$resAvail <- R
+        ind_data$network <- paste("dyadic", (r+1)/2, sep = "_")
+        focaldomDist <- as.vector(unlist(ind_data[domDistribution]))
+        
+        resources <- R * round(mean(colSums(netList[[r+1]])))
+        
+        #Create list for holding output on each time step
+        dataList <- vector("list", 8000)
+        
+        #Set initial time
+        t = 1
+        
+        repeat{
+          
+          #Identify current set of informed nodes
+          informedNodes <- ind_data[which(ind_data$informed == 1),]$id
+          
+          #Each informed individual updates its likelihood of producing a novel behavior by:
+          learningOutcomes <- dyadic_diffusion(ind_data = ind_data, network = r, netList = netList, informedNodes = informedNodes, domValues = focaldomDist)
+          ind_data$active <- learningOutcomes[[1]]
+          ind_data$newLearners <- learningOutcomes[[2]]
+          
+          for(i in 1:nrow(ind_data)) {
+            if(ind_data$active[i] == 1 & ind_data$firstProd[i] == 0) {
+              ind_data$firstProd[i] <- t
+            }
+          }
+          
+          #Record those individuals that acquired the trait and note the time step at which this occurred
+          ind_data$acqTime <- ifelse(ind_data$newLearners == 1 & ind_data$informed == 0, t, ind_data$acqTime)
+          
+          #Add newly informed individuals to the list of informed individuals
+          ind_data$informed <- ifelse(ind_data$informed == 0, 
+                                      ifelse(ind_data$newLearners == 1, 1, 0), 1)
+          
+          dataTemp <- data.table("simID" = s,
+                                 "domDist" = domDistribution,
+                                 "network" = paste("dyadic", (r+1)/2, sep = "_"),
+                                 "resAvail" = R,
+                                 "groupAdjust" = g,
+                                 "nInformed" = 0,
+                                 "percInformed" = 0,
+                                 "nActive" = 0,
+                                 "percActive" = 0
+          )
+          
+          dataTemp$nInformed <- sum(ind_data$informed)
+          dataTemp$percInformed <- dataTemp$nInformed/nrow(ind_data)
+          
+          dataTemp$nActive <- sum(ind_data$active)
+          dataTemp$percActive <- dataTemp$nActive/dataTemp$nInformed
+          
+          dataList[[t]] <- dataTemp
+          
+          #If all individuals are informed or an inordinately long time has elapsed, end the sim
+          if(sum(ind_data$firstProd >0) == nrow(ind_data) | t == 8000) {
+            break
+          }
+          
+          #If the simulation is continuing, advance to the next time step
+          t <- t + 1
+        }
+        
+        dataCombined <- rbindlist(dataList)
+        dataCombined$timeStep <- 1:nrow(dataCombined)
+        
+        diffusionSummary <- data.table("simID" = s, "domDist" = domDistribution,
+                                       "network" = paste("dyadic", (r+1)/2, sep = "_"), "resAvail" = R,
+                                       "groupAdjust" = g,
+                                       "TTD" = max(ind_data$acqTime), "TTFP" = max(ind_data$firstProd),
+                                       "numIndivs" = n_indivs, "numProducers" = 0,
+                                       "orderDiv" = 0,
+                                       "propDiv" = 0,
+                                       "timeDelay" = 0,
+                                       "burstiness" = 0)
+        
+        numProducers <- nrow(ind_data[which(ind_data$firstProd > 0),])
+        orderDiv <- (1/(numProducers - 1)) * 
+          sum(abs(rank(ind_data[which(ind_data$acqTime > 0  & ind_data$firstProd > 0),]$acqTime) - 
+                    rank(ind_data[which(ind_data$acqTime > 0 & ind_data$firstProd > 0),]$firstProd)))
+        propDiv <- sum(rank(ind_data[which(ind_data$firstProd > 0),]$acqTime) != 
+                         rank(ind_data[which(ind_data$firstProd > 0),]$firstProd)) / numProducers
+        timeDelay <- (1/numProducers) * sum(abs(ind_data[which(ind_data$firstProd > 0),]$acqTime - 
+                                                  ind_data[which(ind_data$firstProd > 0),]$firstProd))
+        
+        diffusionSummary$numProducers <- numProducers
+        diffusionSummary$orderDiv <- orderDiv
+        diffusionSummary$propDiv <- propDiv
+        diffusionSummary$timeDelay <- timeDelay
+        
+        intervalData <- sapply(1:(length(ind_data$acqTime) - 1), function(x) sort(ind_data$acqTime)[x+1] - sort(ind_data$acqTime)[x])
+        diffusionSummary$burstiness <- (sd(intervalData)-mean(intervalData))/(sd(intervalData) + mean(intervalData))
+        
+        fwrite(ind_data, file = file.path(sim_indData, sprintf("simData_%s_%.01i_%s_%.02f_%s_%s.csv", run_ID, s, paste("hyper", (r+1)/2, sep = "_"), R, domDistribution, g)))
+        fwrite(dataCombined, file = file.path(sim_summaryData, sprintf("simData_%s_%.01i_%s_%.02f_%s_%s.csv", run_ID, s, paste("hyper", (r+1)/2, sep = "_"), R, domDistribution, g)))
+        fwrite(diffusionSummary, file = file.path(sim_diffSummaryData, sprintf("simData_%s_%.01i_%s_%.02f_%s_%s.csv", run_ID, s, paste("hyper", (r+1)/2, sep = "_"), R, domDistribution, g)))
+        
+        } 
+      }
+      for(r in c(2,4,6)) {
+        for(g in groupAdjust) {
+        #Reset individual-level data to initial values
+        ind_data <- ind_dataOrig
+        
+        domDistribution <- domDist[d]
+        ind_data$distUsed <- domDistribution
+        ind_data$resAvail <- R
+        ind_data$network <- paste("hyper", r/2, sep = "_")
+        focaldomDist <- as.vector(unlist(ind_data[domDistribution]))
+        
+        #Create list for holding output on each time step
+        dataList <- vector("list", 8000)
+        
+        resources <- R * round(mean(colSums(netList[[r]])))
+        
+        #Set initial time
+        t = 1
+        
+        repeat{
+          
+          #Identify current set of informed nodes
+          informedNodes <- ind_data[which(ind_data$informed == 1),]$id
+          
+          #Each informed individual updates its likelihood of producing a novel behavior by:
+          learningOutcomes <- hyperNetwork_diffusion(ind_data = ind_data, network = r, netList = netList, resources = resources,
+                                                              informedNodes = informedNodes, domValues = focaldomDist)
+          ind_data$active <- learningOutcomes[[1]]
+          ind_data$newLearners <- learningOutcomes[[2]]
+          
+          for(i in 1:nrow(ind_data)) {
+            if(ind_data$active[i] == 1 & ind_data$firstProd[i] == 0) {
+              ind_data$firstProd[i] <- t
+            }
+          }
+          
+          #Record those individuals that acquired the trait and note the time step at which this occurred
+          ind_data$acqTime <- ifelse(ind_data$newLearners == 1 & ind_data$informed == 0, t, ind_data$acqTime)
+          
+          #Add newly informed individuals to the list of informed individuals
+          ind_data$informed <- ifelse(ind_data$informed == 0, 
+                                      ifelse(ind_data$newLearners == 1, 1, 0), 1)
+          
+          dataTemp <- data.table("simID" = s,
+                                 "domDist" = domDistribution,
+                                 "network" = paste("hyper", r/2, sep = "_"),
+                                 "resAvail" = R,
+                                 "groupAdjust" = g,
+                                 "nInformed" = 0,
+                                 "percInformed" = 0,
+                                 "nActive" = 0,
+                                 "percActive" = 0
+          )
+          
+          dataTemp$nInformed <- sum(ind_data$informed)
+          dataTemp$percInformed <- dataTemp$nInformed/nrow(ind_data)
+          
+          dataTemp$nActive <- sum(ind_data$active)
+          dataTemp$percActive <- dataTemp$nActive/dataTemp$nInformed
+          
+          dataList[[t]] <- dataTemp
+          
+          #If all individuals are informed or an inordinately long time has elapsed, end the sim
+          if(sum(ind_data$firstProd >0) == nrow(ind_data) | t == 8000) {
+            break
+          }
+          
+          #If the simulation is continuing, advance to the next time step
+          t <- t + 1
+        }
+        
+        dataCombined <- rbindlist(dataList)
+        dataCombined$timeStep <- 1:nrow(dataCombined)
+        
+        diffusionSummary <- data.table("simID" = s, "domDist" = domDistribution,
+                                       "network" = paste("hyper", r/2, sep = "_"), "resAvail" = R,
+                                       "groupAdjust" = g,
+                                       "TTD" = max(ind_data$acqTime), "TTFP" = max(ind_data$firstProd),
+                                       "numIndivs" = n_indivs, "numProducers" = 0,
+                                       "orderDiv" = 0,
+                                       "propDiv" = 0,
+                                       "timeDelay" = 0,
+                                       "burstiness" = 0)
+        
+        numProducers <- nrow(ind_data[which(ind_data$firstProd > 0),])
+        orderDiv <- (1/(numProducers - 1)) * 
+          sum(abs(rank(ind_data[which(ind_data$acqTime > 0  & ind_data$firstProd > 0),]$acqTime) - 
+                    rank(ind_data[which(ind_data$acqTime > 0 & ind_data$firstProd > 0),]$firstProd)))
+        propDiv <- sum(rank(ind_data[which(ind_data$firstProd > 0),]$acqTime) != 
+                         rank(ind_data[which(ind_data$firstProd > 0),]$firstProd)) / numProducers
+        timeDelay <- (1/numProducers) * sum(abs(ind_data[which(ind_data$firstProd > 0),]$acqTime - 
+                                                  ind_data[which(ind_data$firstProd > 0),]$firstProd))
+        
+        diffusionSummary$numProducers <- numProducers
+        diffusionSummary$orderDiv <- orderDiv
+        diffusionSummary$propDiv <- propDiv
+        diffusionSummary$timeDelay <- timeDelay
+        
+        intervalData <- sapply(1:(length(ind_data$acqTime) - 1), function(x) sort(ind_data$acqTime)[x+1] - sort(ind_data$acqTime)[x])
+        diffusionSummary$burstiness <- (sd(intervalData)-mean(intervalData))/(sd(intervalData) + mean(intervalData))
+        
+        fwrite(ind_data, file = file.path(sim_indData, sprintf("simData_%s_%.01i_%s_%.02f_%s_%s.csv", run_ID, s, paste("hyper", r/2, sep = "_"), R, domDistribution ,g)))
+        fwrite(dataCombined, file = file.path(sim_summaryData, sprintf("simData_%s_%.01i_%s_%.02f_%s_%s.csv", run_ID, s, paste("hyper", r/2, sep = "_"), R, domDistribution ,g)))
+        fwrite(diffusionSummary, file = file.path(sim_diffSummaryData, sprintf("simData_%s_%.01i_%s_%.02f_%s_%s.csv", run_ID, s, paste("hyper", r/2, sep = "_"), R, domDistribution ,g)))
+        
+        }
+      }
+    }
+  }
+}
+###############################################
---
+++ hypernetwork_group_size_vs_composition_simFunctions.R
@@ -0,0 +1,209 @@
+##Generate individuals in family/kinship groups and spatially embed them in x-y grid
+generate_population <- function(popSize, nInitInformed) {
+  #Assign IDs
+  id <- seq(1, popSize, 1)
+  
+  #Assign individual coordinates
+  ind_x <- ind_y <- rep(NA, popSize)
+  for(i in 1:popSize) {
+    ind_x[i] <- runif(1, 0, 1)
+    ind_y[i] <- runif(1, 0, 1)
+  }
+  
+  ind_data <- data.frame(id,ind_x,ind_y)
+  
+  #Create an initial informed individual
+  ind_data$informed <- 0
+  ind_data$informed[sample(1:nrow(ind_data), nInitInformed, replace = FALSE)] <- 1
+  
+  #Identify initial informed nodes so that initial seed can be denoted
+  informedNodes <- ind_data[which(ind_data$informed == 1),]$id
+  ind_data$initInformed <- 0
+  ind_data$initInformed[informedNodes] <- 1
+  ind_data$acqTime <- 0
+  ind_data$firstProd <- 0
+  
+  return(ind_data)
+}
+
+##Create spatial proximity network and hypergraphs
+generate_latent_space_multilayer_hypergraph <- function(ind_data, r) {
+  #Calculate distance matrix
+  dist_mat <- as.matrix(dist(cbind(ind_data$ind_x, ind_data$ind_y)))
+  diag(dist_mat) <- 0
+  
+  #Set initial threshold to use for calculating dyadic layer and intermediate layer (i.e., hypernetwork 1)
+  thresh <- r[1]
+  
+  #Create matrix of dyadic associations
+  sp_mat <- dist_mat < thresh
+  sp_net <- apply(sp_mat, 2, as.numeric)
+  diag(sp_net) <- 0
+  sp_graph <- graph_from_adjacency_matrix(sp_net, mode="max")
+  
+  #The following procedure ensures that a single connected component is formed
+  C1 <- components(sp_graph)
+  numComps <- max(C1$membership)
+  
+  if(numComps > 1) {
+    
+    #Create data.table to collect nearest distances among network components
+    newEdgeList <- data.table("Comp1" = unlist(sapply(1:(numComps-1), function(x) rep(x,(numComps-x)))),
+                              "Comp2" = unlist(sapply(1:(numComps-1), function(x) (x+1):numComps)),
+                              "ID1" = 0,
+                              "ID2" = 0,
+                              "Dist" = 0)
+    
+    #For each pair of network components, identify the pair of nodes that are physically closest
+    for(i in 1:(numComps-1)) {
+      for(j in 2:numComps) {
+        memb1 <- c(which(C1$membership == i))
+        memb2 <- c(which(C1$membership == j))
+        tempMat <- sapply(memb1, function(x) dist_mat[memb2,x])
+        newEdgeList[Comp1 == i & Comp2 == j]$Dist <- min(tempMat)
+        targetEdge <- which(tempMat == min(tempMat))
+        if(C1$csize[i] > 1 & C1$csize[j] > 1){
+          newEdgeList[Comp1 == i & Comp2 == j]$ID1 <- as.vector(memb1[ceiling(targetEdge/dim(tempMat)[1])])
+          newEdgeList[Comp1 == i & Comp2 == j]$ID2 <- as.vector(memb2[targetEdge-(dim(tempMat)[1] * (ceiling(targetEdge/dim(tempMat)[1])-1))])
+        } else{
+          if(C1$csize[i] > 1 & C1$csize[j] == 1) {
+            newEdgeList[Comp1 == i & Comp2 == j]$ID2 <- memb2
+            newEdgeList[Comp1 == i & Comp2 == j]$ID1 <- memb1[targetEdge]
+          } else {
+            newEdgeList[Comp1 == i & Comp2 == j]$ID1 <- memb1
+            newEdgeList[Comp1 == i & Comp2 == j]$ID2 <- memb2[targetEdge]
+          }
+        }
+      }
+    }
+    newEdgeList <- newEdgeList[order(newEdgeList$Dist)]
+    
+    #Iterate through the new potential edges in order of shortest distance
+    #A new edge is added one at a time and the number of components in the resulting network assessed
+    #Stop the process once a single connected component has been formed
+    i = 1
+    repeat{
+      sp_net[newEdgeList$ID1[i], newEdgeList$ID2[i]] <- sp_net[newEdgeList$ID2[i], newEdgeList$ID1[i]] <- 1
+      if(components(graph_from_adjacency_matrix(sp_net, mode ="max"))$no == 1) {
+        break}
+      i <- i + 1
+    }
+    
+    #Create new dyadic spatial proximity network
+    sp_graph <- graph_from_adjacency_matrix(sp_net,mode="max")
+    
+  }
+  
+  #Create 1st higher-order layer (also referred to as the 'intermediate' layer (after Grueter et al. 2020 TREE))
+  #Each maximal clique using the initial spatial proximity threshold is assigned as a hyperedge
+  subgroups <- max_cliques(sp_graph)
+  
+  sp_hyp1 <- matrix(0, nrow = nrow(ind_data), ncol = length(subgroups))
+  
+  for(i in 1:length(subgroups)) {
+    sp_hyp1[c(as.vector(subgroups[[i]])),i] <- 1 
+  }
+  
+  sp_dyNet1 <- get_network(association_data = t(sp_hyp1), data_format = "GBI", association_index = "SRI")
+  
+  #Create higher-order layer 2 (also referred to as the 'upper' layer)
+  thresh2 <- r[2]
+  
+  #Create matrix of subgroup membership
+  sp_mat2 <- dist_mat < thresh2
+  sp_net2 <- apply(sp_mat2, 2, as.numeric)
+  sp_graph2 <- graph_from_adjacency_matrix(sp_net2, mode="max")
+  
+  #Create hypernetwork as above
+  subgroups2 <- max_cliques(sp_graph2)
+  sp_hyp2 <- matrix(0, nrow = nrow(ind_data), ncol = length(subgroups2))
+  for(i in 1:length(subgroups2)) {
+    sp_hyp2[c(as.vector(subgroups2[[i]])),i] <- 1 
+  }
+  
+  sp_dyNet2 <- get_network(association_data = t(sp_hyp2), data_format = "GBI", association_index = "SRI")
+  
+  #Create higher-order layer 3 (also referred to as the 'apex' layer)
+  thresh3 <- r[3]
+  
+  #Create matrix of subgroup membership
+  sp_mat3 <- dist_mat < thresh3
+  sp_net3 <- apply(sp_mat3, 2, as.numeric)
+  sp_graph3 <- graph_from_adjacency_matrix(sp_net3, mode="max")
+  
+  subgroups3 <- max_cliques(sp_graph3)
+  sp_hyp3 <- matrix(0, nrow = nrow(ind_data), ncol = length(subgroups3))
+  
+  for(i in 1:length(subgroups3)) {
+    sp_hyp3[c(as.vector(subgroups3[[i]])),i] <- 1 
+  }
+  
+  sp_dyNet3 <- get_network(association_data = t(sp_hyp3), data_format = "GBI", association_index = "SRI")
+  
+  return(list(sp_dyNet1, sp_hyp1, sp_dyNet2, sp_hyp2, sp_dyNet3, sp_hyp3))
+}
+
+hyperNetwork_diffusion <- function(ind_data, netList, network, informedNodes, domValues, resources, groupAdjustment = FALSE) {
+  
+  produceTemp <- rep(0, nrow(ind_data))
+  newLearner <- rep(0, nrow(ind_data))
+  
+  for(i in informedNodes) {
+    focalHyperedge <- ifelse(length(which(netList[[network]][i,]>0)) > 1, sample(which(netList[[network]][i,]>0),1), which(netList[[network]][i,]>0))
+    focalHyperedgeMembs <- which(netList[[network]][,focalHyperedge]>0)
+    currentInformed <- focalHyperedgeMembs[focalHyperedgeMembs %in% informedNodes]
+    
+    if(length(currentInformed)==1){
+      demons <- currentInformed
+    } else{
+      demons <- unique(sample(currentInformed, resources, replace = TRUE, 
+                              prob = domValues[currentInformed]))
+    }
+    
+    gA <- ifelse(groupAdjustment, length(focalHyperedgeMembs)-length(demons), 1)
+    produceTemp[demons] <- 1
+    currentUninformed <- focalHyperedgeMembs[!(focalHyperedgeMembs %in% currentInformed)]
+    learningOutcomes <- runif(length(currentUninformed)) < (1 - (1 - social_trans/gA)^length(demons))
+    
+    if(length(currentUninformed[learningOutcomes]) > 0) {
+      newLearner[currentUninformed[learningOutcomes]] <- 1
+    }
+  }
+  
+  return(list(produceTemp,newLearner))
+}
+
+###OLD
+#learningOutcomes <- runif(length(currentUninformed)) < (social_trans * length(demons)/(length(focalHyperedgeMembs)-length(demons)))
+
+dyadic_diffusion <- function(ind_data, netList, network, informedNodes, domValues, resources, groupAdjustment = FALSE) {
+  
+  produceTemp <- rep(0, nrow(ind_data))
+  focalGraph <- graph_from_adjacency_matrix(netList[[network]], weighted = TRUE, mode = c("undirected"))
+  
+  for(i in informedNodes) {
+    focalNeighbors <- as.vector(neighbors(focalGraph, v = i))
+    currentInformed <- c(i, focalNeighbors[focalNeighbors %in% informedNodes])
+    
+    if(length(currentInformed) == 1) {
+      demons <- currentInformed
+    } else {
+      demons <- unique(sample(currentInformed, resources, replace = TRUE, 
+                              prob = domValues[currentInformed] * 
+                                c(1,netList[[network]][i,focalNeighbors[focalNeighbors %in% informedNodes]])))
+    }
+    produceTemp[demons] <- 1
+  }
+  
+  if(groupAdjustment) {
+    acqProb <- sapply(1:nrow(ind_data), function(x) 1-prod(1-(produceTemp*netList[[network]][x,]*social_trans)/
+                                                             (1+sum(ifelse(ind_data$informed==1,0,1)*netList[[network]][x,]))))
+  } else{
+    acqProb <- sapply(1:nrow(ind_data), function(x) 1-prod(1-produceTemp*netList[[network]][x,]*social_trans))
+  }
+  newLearner <- ifelse(acqProb > runif(nrow(ind_data), min = 0, max = 1), 1, 0)
+  newLearner[informedNodes] <- 0
+  
+  
+  return(list(produceTemp,newLearner))
+}
